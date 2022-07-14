c658e3987ad997f5b2cebb8c0cf350d933b2d1e2
Robby <spahatticat246@gmail.com>
2022-07-13 20:12:28 -0400

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 baseURL=document.URL;
 function search() {
-    let search_bar = document.getElementById(searchbar);
+    let search_bar = document.getElementById("searchbar").value;
     console.log(search_bar);
     window.location.replace(`${baseURL}?search=${search_bar}`);
 }
--- search.php
+++ search.php
@@ -1,37 +1,57 @@
-<?php
-//$sql = "SELECT * FROM searces WHERE frontname =";
-//if($result = mysqli_query($link, $sql)){
-//    if(mysqli_num_rows($result) > 0){
-//        echo "<table>";
-//            echo "<tr>";
-//                echo "<th>id</th>";
-//            echo "<th>first_name</th>";
-//              echo "<th>last_name</th>";
-//              echo "<th>email</th>";
-//              echo "</tr>";
-//    while($row = mysqli_fetch_array($result)){
-//          echo "<tr>";
-//              echo "<td>" . $row['id'] . "</td>";
-//              echo "<td>" . $row['first_name'] . "</td>";
-//              echo "<td>" . $row['last_name'] . "</td>";
-//              echo "<td>" . $row['email'] . "</td>";
-//          echo "</tr>";
-//      }
-//      echo "</table>";
-//      // Free result set
-//      mysqli_free_result($result);
-?>
 <!DOCTYPE html>
+<?php 
+require "C:\Users\Robby Davis\Documents\DevSearch\db-connect.php";
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript">
+    baseURL=document.URL;
+    function search() {
+        let search_bar = document.getElementById("searchbar").value;
+        console.log(search_bar);
+        const URL = baseURL.split('?');
+        window.location.replace(`${URL[0]}?sb=${search_bar}`);
+    }
+</script>
 </head>
 <body>
 <h1>Search</h1>
-<input type="text" id="searchbar"></input><input type="button" value="Search" id="searchbutton" onclick="search()"></input>
+<input type="text" id="searchbar" name="sb"></input><input type="button" value="Search" id="searchbutton" onclick="search()"></input>
+<h3>Results:</h3>
+<hr></hr>
+<?php
+$sql = "SELECT * FROM searces WHERE frontname='" . $_GET['sb']."'";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id:</th>";
+            echo "<th>Name:</th>";
+              echo "<th>Backend-name:</th>";
+              echo "<th>URL:</th>";
+              echo "</tr>";
+    }
+    while($row = mysqli_fetch_array($result)){
+          $url = $row['daURL'];
+          $name = $row['frontName'];
+          echo "<tr>";
+              echo "<td>" . $row['id'] . "</td>";
+              echo "<td>" . $row['frontName'] . "</td>";
+              echo "<td>" . $row['backName'] . "</td>";
+              echo "<td>" . $row['daURL'] . "</td></a>";
+          echo "</tr>";
+      }
+      echo "</table>";
+       // Free result set
+      mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found, or there was an error\nerror:". mysqli_error($link);
+    }
+?>
+<a href="<?php $url ?>"><?php $name?></a>
 </body>
 </html>
